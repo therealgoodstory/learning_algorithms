026b33bb889cda7fbab5b69c7ae266df53257a34
Epimetheus <therealgoodstory@gmail.com>
2021-05-26 21:33:39 +0300

    update Binary Search algorithm

--- src/components/BinarySearch.tsx
+++ src/components/BinarySearch.tsx
@@ -1,32 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import '../styles/binarySearch.css'
 import Navigation from './Navigation'
+
 
 const BinarySearch = () => {
   const [result, setResult] = useState("")
   const [x, setX] = useState(0)
+  const [allSteps, setAllSteps] = useState<number[]>([])
 
   const arr = new Array(10000000).fill(0).map((_, id) => id + 1)
 
   const onClick = () => {
+    // переделать всё на const && избежать цикла
     let low = 0
     let high =  arr.length - 1
     let middle = Math.floor((low + high) / 2)
     let counter = 0
+    let steps = []
   
     while (x !== arr[middle]) {
       middle = Math.floor((low + high) / 2)
       counter += 1
+      steps.push(arr[middle])
       if (x > arr[middle]) { low = middle }
       if (x < arr[middle]) { high = middle }
       if (x < arr[low] || x > arr[high]) {
         break
       }
     }
+  
     if (x < arr[low] || x > arr[high]) {
       setResult('Введённое число отсутствует')
     } else {
       setResult(`Число найденно за ${counter} итераций!`)
+      setAllSteps(steps)
     }
   }
 
@@ -51,6 +58,9 @@ const BinarySearch = () => {
         <span className="main__result">
           {result}
         </span>
+        <div className="step-field">
+          {allSteps.map((step, idx) => <span className="step-field__text" key={idx}>{step}</span>)}
+        </div>
       </section>
     </main>
   );
